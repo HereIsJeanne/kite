05abbeda177b9995c103d55d8f62b6f16295e95e
Dario Spina <dariospina@Darios-MBP.broadband>
2023-06-28 16:52:20 +0100

    change routes

--- next.config.js
+++ next.config.js
@@ -1,10 +1,21 @@
 module.exports = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'https://sheetdb.io/api/v1/5uxyugigju92s/:path*',
-      },
-    ];
+  exportPathMap: async function() {
+    const paths = {
+      "/": { page: "/" },
+      "/about": { page: "/about" },
+      "/sp50": { page: "/sp50" },
+      // other static routes...
+    };
+
+    // Assume your latest ID is 10
+    const latestId = 5;
+
+    // Create routes for each id
+    for (let id = 1; id <= latestId; id++) {
+      paths[`/sp50/${id}`] = { page: `/sp50/[id]`, query: { id } };
+    }
+
+    return paths;
   },
+  assetPrefix: './',
 };
