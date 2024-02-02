cc764a9cc9aeb476aeb4228be1abf78415b9888a
nuclear-pasta <dario.spina@seedrs.com>
2024-02-02 17:27:49 +0000

    test

--- api/cacheImage.js
+++ api/cacheImage.js
@@ -1,23 +1,24 @@
 // api/cacheImage.js
 
 export default async (req, res) => {
-    const { imageUrl } = req.query;
+    const imageUrl = decodeURIComponent(req.query.imageUrl);
 
     try {
+        console.log(`Fetching image: ${imageUrl}`);
         const response = await fetch(imageUrl);
         if (!response.ok) {
-            throw new Error(`Failed to fetch the image: ${response.statusText}`);
+            throw new Error(`Failed to fetch the image. Status: ${response.statusText}`);
         }
 
-        const imageBuffer = await response.buffer();
+        // Use arrayBuffer() instead of buffer()
+        const imageArrayBuffer = await response.arrayBuffer();
+        const imageBuffer = Buffer.from(imageArrayBuffer);
 
-        // Set cache headers for edge caching
         res.setHeader('Cache-Control', 'public, max-age=0, s-maxage=86400, stale-while-revalidate');
-        res.setHeader('Content-Type', 'image/jpeg'); // Adjust content type as needed
-
+        res.setHeader('Content-Type', 'image/jpeg'); // Adjust as needed
         res.send(imageBuffer);
     } catch (error) {
-        console.error(error);
-        res.status(500).send('Error fetching image');
+        console.error(`Error fetching image from ${imageUrl}:`, error);
+        res.status(500).send(`Error fetching image: ${error.message}`);
     }
 };
--- spf50Data.js
+++ spf50Data.js
@@ -4,13 +4,18 @@ export async function fetchSpf50Data() {
   if (cachedSpf50Data.length === 0) {
     const res = await fetch('https://sheetdb.io/api/v1/pbw45bqdwlytn');
     const data = await res.json();
-    cachedSpf50Data = data;
+
+    // Modify each image URL to route through the cacheImage serverless function
+    cachedSpf50Data = data.map(item => ({
+      ...item,
+      Image: `/api/cacheImage?imageUrl=${encodeURIComponent(item.Image)}`
+    }));
   }
   return cachedSpf50Data;
 }
 
 export async function fetchSpf50Ids() {
-    const spf50Data = await fetchSpf50Data();
-    const ids = spf50Data.map((item) => item.ID.toString());
-    return ids;
-  }
+  const spf50Data = await fetchSpf50Data();
+  const ids = spf50Data.map((item) => item.ID.toString());
+  return ids;
+}
