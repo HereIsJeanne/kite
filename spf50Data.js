4e955d62ee903e1c27b3ea22e5a696f6597b8409
HereIsJeanne <116828844+HereIsJeanne@users.noreply.github.com>
2024-10-16 19:15:41 +0100

    Update spf50Data.js

--- spf50Data.js
+++ spf50Data.js
@@ -1,16 +1,11 @@
-let cachedSpf50Data = [];
-
 export async function fetchSpf50Data() {
-  if (cachedSpf50Data.length === 0) {
-    const res = await fetch('https://sheetdb.io/api/v1/pbw45bqdwlytn');
-    const data = await res.json();
-    cachedSpf50Data = data;
-  }
-  return cachedSpf50Data;
+  const res = await fetch('https://sheetdb.io/api/v1/pbw45bqdwlytn');
+  const data = await res.json();
+  return data;
 }
 
 export async function fetchSpf50Ids() {
-    const spf50Data = await fetchSpf50Data();
-    const ids = spf50Data.map((item) => item.ID.toString());
-    return ids;
-  }
+  const spf50Data = await fetchSpf50Data();
+  const ids = spf50Data.map((item) => item.ID.toString());
+  return ids;
+}
