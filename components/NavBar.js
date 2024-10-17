2d34312568c0e9ec1a060872e1e6eb929951af51
Dario <40305104+nuclear-pasta@users.noreply.github.com>
2024-10-17 14:45:02 +0100

    Update NavBar.js

--- components/NavBar.js
+++ components/NavBar.js
@@ -10,15 +10,15 @@ function NavBar({ firstItem }) {
 
   return (
     <>
-      <nav className={${styles.navBar} ${styles.mobile}}>
+      <nav className={`${styles.navBar} ${styles.mobile}`}>
         <ul className={styles.navList}>
           <li>SPF50 COLLECTION</li>
           <li>{currentPage === '/about' ? <Link href="/"><MdClose /></Link> : <Link href="/about"><MdInfoOutline /></Link>}</li>
         </ul>
       </nav>
       
-      <nav className={${styles.navBar} ${styles.desktop}}>
-        <ul className={${styles.navList} ${styles.navListDesktop}}>
+      <nav className={`${styles.navBar} ${styles.desktop}`}>
+        <ul className={`${styles.navList} ${styles.navListDesktop}`}>
           <li>{firstItem ? <span className={styles.left}>{firstItem}</span> : <Link className={styles.left} href="/">FLIGHT MODE</Link>}</li>
           <li><Link className={styles.center} id="center" href="/spf50">SPF50 COLLECTION</Link></li>
           <li>  <Link href="/about" className={styles.right}>ABOUT</Link></li>
